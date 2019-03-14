5d82e2de0e0435f6944092f75716d5d854c21992
Bihang Ni <allandtu1989@gmail.com>
2019-03-14 13:04:40 +0100

    Implement desktop contact application

--- DesktopContractApp/MainWindow.xaml.cs
+++ DesktopContractApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesktopContractApp.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,51 @@ namespace DesktopContractApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Contact> contacts;
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();     
+            contacts = new List<Contact>();
+            ReadDatabase();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+            ReadDatabase();
+        }
+        void ReadDatabase()
+        {
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contact>();
+                contacts = (conn.Table<Contact>().ToList()).OrderBy(c => c.Name).ToList();
+            }
+
+            if (contacts != null)
+            {
+                contactListView.ItemsSource = contacts;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox searchTexBox = sender as TextBox;
+            var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTexBox.Text.ToLower())).ToList();
+            contactListView.ItemsSource = filteredList;
+        }
+
+        private void ContactListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Contact selectedContact = (Contact)contactListView.SelectedItem;
+            if (selectedContact != null)
+            {
+                ContactDetailsWindow contactDetailsWindow = new ContactDetailsWindow(selectedContact);
+                contactDetailsWindow.ShowDialog();
+                ReadDatabase();
+            }
         }
     }
 }
